c850f4def5f039e5150811369e5ebab05750d215
unknown <igormatheus.0702@gmail.com>
2024-11-10 04:27:32 -0300

    att 1.4 primeiro passo controladores registro alunos

---
+++ src/main/java/poowyden/poowyden/ItemAluno.java
@@ -0,0 +1,28 @@
+/*
+package poowyden.poowyden;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ItemAluno implements Initializable {
+    @FXML
+    private Label contato;
+
+    @FXML
+    private ImageView img;
+
+    @FXML
+    private Label nomeUser;
+
+    @FXML
+    private Label statusPagamento;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {}
+}
+*/
--- src/main/java/poowyden/poowyden/RegistroAluno.java
+++ src/main/java/poowyden/poowyden/RegistroAluno.java
@@ -1,4 +1,31 @@
+/*
 package poowyden.poowyden;
 
-public class RegistroAluno {
+import poowyden.poowyden.ItemAluno;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class RegistroAluno implements Initializable {
+    @FXML
+    private VBox alunosLayout;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    private List<ItemAluno> contacts(){
+        List<ItemAluno> ls = new ArrayList<>();
+        ItemAluno contact = new ItemAluno();
+
+        contact.nomeUser("Igor Matheus");
+        contact.img("/img/person.png");
+    }
 }
+*/
