ecbcfca60d12b29d2e9829bd6bb27af0be36e5dc
Henrique Gouveia <henriquesgo42@gmail.com>
2024-11-11 15:09:15 -0300

    requires java.sql

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module poowyden.poowyden {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.sql;
 
 
     opens poowyden.poowyden to javafx.fxml;
