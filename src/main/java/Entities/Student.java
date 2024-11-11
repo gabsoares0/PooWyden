fd0553301e559b7d1c4322d9b13eb057dbdcaea0
Henrique Gouveia <henriquesgo42@gmail.com>
2024-11-11 15:08:11 -0300

    Inserção do atributo id, getId() e setId()

--- src/main/java/Entities/Student.java
+++ src/main/java/Entities/Student.java
@@ -1,11 +1,14 @@
 package Entities;
 
 public class Student {
+    private int id;
     private String name;
     private String phone;
     private String imgSrc;
     private String dataNasc;
 
+    public int getId() {return id;}
+    public void setId(int id) {this.id = id;}
     public String getName() {
         return name;
     }
