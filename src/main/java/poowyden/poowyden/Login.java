34951ba4d4565e8b5351e84e0db13ac22c85ae59
Gabriel <gabriieellxeu@gmail.com>
2024-11-14 19:09:09 -0300

    Nova interface de login

--- src/main/java/poowyden/poowyden/Login.java
+++ src/main/java/poowyden/poowyden/Login.java
@@ -8,12 +8,19 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Login extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 350);
+        Scene scene = new Scene(fxmlLoader.load());
+
         stage.setTitle("Login");
         stage.setScene(scene);
+
+        // Configura a tela de login para abrir maximizada
+        stage.setMaximized(true); // Abre em modo maximizado (ocupando a tela toda)
+        // stage.setFullScreen(true); // Abre em modo tela cheia (ocultando a barra de título)
+
         stage.show();
     }
 
