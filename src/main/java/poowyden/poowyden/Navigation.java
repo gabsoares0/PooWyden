229eed2f5beb2f429f9fb08699a1ecc87b2b1b2b
Henrique Gouveia <henriquesgo42@gmail.com>
2024-11-25 13:46:43 -0300

    Implementacao da classe que contem o metodo que carrega o dashboard.fxml com o initialView como argumento

--- src/main/java/poowyden/poowyden/Navigation.java
+++ src/main/java/poowyden/poowyden/Navigation.java
@@ -1,4 +1,31 @@
 package poowyden.poowyden;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Navigation {
+    public void openDashboard(String initialView){
+        try{
+            //Carrega o FXML do dashboard
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("dashboard.fxml"));
+            Scene scene = new Scene(loader.load());
+
+            //Obtem o controlador e configura a visão inicial
+            DashboardController controller = loader.getController();
+            controller.setInitialView(initialView);
+
+            //Configura o placo e exibe a cena
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Dashboard");
+            stage.show();
+
+        }catch(IOException e){
+            System.err.println("Erro ao carregar o dashboard.fxml");
+            e.printStackTrace();
+        }
+    }
 }
