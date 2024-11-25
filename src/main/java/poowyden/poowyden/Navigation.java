7b3f5ab8b631887a13a3894b59dca543ad8552d9
Henrique Gouveia <henriquesgo42@gmail.com>
2024-11-25 14:07:34 -0300

    Implementacao de classe que permite a navegação sempre aproveitando o mesmo Stage

--- src/main/java/poowyden/poowyden/Navigation.java
+++ src/main/java/poowyden/poowyden/Navigation.java
@@ -7,23 +7,23 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Navigation {
-    public void openDashboard(String initialView){
-        try{
-            //Carrega o FXML do dashboard
+
+    public void openDashboard(Stage currentStage, String initialView) {
+        try {
+            // Carrega o FXML do dashboard
             FXMLLoader loader = new FXMLLoader(getClass().getResource("dashboard.fxml"));
             Scene scene = new Scene(loader.load());
 
-            //Obtem o controlador e configura a visão inicial
+            // Obtém o controlador e configura a visão inicial
             DashboardController controller = loader.getController();
             controller.setInitialView(initialView);
 
-            //Configura o placo e exibe a cena
-            Stage stage = new Stage();
-            stage.setScene(scene);
-            stage.setTitle("Dashboard");
-            stage.show();
+            // Configura o Stage existente com a nova cena
+            currentStage.setScene(scene);
+            currentStage.setTitle("Dashboard");
+            currentStage.show();
 
-        }catch(IOException e){
+        } catch (IOException e) {
             System.err.println("Erro ao carregar o dashboard.fxml");
             e.printStackTrace();
         }
