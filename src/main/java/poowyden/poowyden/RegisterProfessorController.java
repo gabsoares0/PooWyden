2a22d1e54da99bd073a96c6f2d3c3f1823a74ae3
Henrique Gouveia <henriquesgo42@gmail.com>
2024-11-25 13:47:59 -0300

    Mapeamento dos campos do cadastro de professores

--- src/main/java/poowyden/poowyden/RegisterProfessorController.java
+++ src/main/java/poowyden/poowyden/RegisterProfessorController.java
@@ -1,4 +1,53 @@
 package poowyden.poowyden;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class RegisterProfessorController {
+
+    @FXML
+    private Button Cancel;
+
+    @FXML
+    private Button Register;
+
+    @FXML
+    private TextField cityField;
+
+    @FXML
+    private TextField cpfField;
+
+    @FXML
+    private TextField dateOfBirthField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField phoneField;
+
+    @FXML
+    private TextField photoField;
+
+    @FXML
+    private TextField stateField;
+
+    @FXML
+    private TextField streetField;
+
+    @FXML
+    private TextField streetNumberField;
+
+    @FXML
+    void cadastrarAluno(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancelarCadastro(ActionEvent event) {
+
+    }
+
 }
