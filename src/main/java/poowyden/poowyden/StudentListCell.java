221aba225defa7ae8ce3e9bfd147675ad6bf4b31
Henrique Gouveia <henriquesgo42@gmail.com>
2024-11-27 15:35:41 -0300

    Implementacao da sobreescricao do metodo updateItem, definido na classe ListCell do Javafx

--- src/main/java/poowyden/poowyden/StudentListCell.java
+++ src/main/java/poowyden/poowyden/StudentListCell.java
@@ -8,4 +8,28 @@ import javafx.scene.text.Text;
 //ListCell<T> é uma classe genérica do JavaFX que representa uma única célula em um ListView,
 //T é o tipo de objeto que a célula exibirá
 public class StudentListCell extends ListCell<Student> {
+    private GridPane gridPane = new GridPane();
+    private Text name = new Text();
+    private Text phone = new Text();
+    private Text dataNasc = new Text();
+
+    public StudentListCell() {
+        gridPane.add(name, 0, 0);
+        gridPane.add(phone, 1, 0);
+        gridPane.add(dataNasc, 2, 0);
+        gridPane.setHgap(50); //Espaçamento horizontal entre colunas
+    }
+
+    @Override
+    protected void updateItem(Student student, boolean empty) {
+        super.updateItem(student, empty);
+        if (empty || student == null) {
+            setGraphic(null);
+        }else{
+            name.setText(student.getName());
+            phone.setText(student.getPhone());
+            dataNasc.setText(student.getDataNasc());
+            setGraphic(gridPane);
+        }
+    }
 }
