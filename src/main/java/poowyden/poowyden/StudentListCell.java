731b918b0c3b41ab052d27adcc15d51df93b78f3
Henrique Gouveia <henriquesgo42@gmail.com>
2024-11-27 15:33:42 -0300

    Implementacao da classe que coloca os itens em colunas na lista vertical de estudantes

---
+++ src/main/java/poowyden/poowyden/StudentListCell.java
@@ -0,0 +1,11 @@
+package poowyden.poowyden;
+
+import Entities.Student;
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
+//ListCell<T> é uma classe genérica do JavaFX que representa uma única célula em um ListView,
+//T é o tipo de objeto que a célula exibirá
+public class StudentListCell extends ListCell<Student> {
+}
--- src/main/java/poowyden/poowyden/StudentsController.java
+++ src/main/java/poowyden/poowyden/StudentsController.java
@@ -3,63 +3,62 @@ package poowyden.poowyden;
 import DAO.StudentDAOImpl;
 import Entities.Student;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.collections.ObservableList;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 import java.io.IOException;
 import java.util.List;
 
-
 public class StudentsController {
 
-    // Referência ao botão no FXML
     @FXML
     private Button Registrar;
 
     @FXML
-    private ListView<String> studentsListView;
+    private ListView<Student> studentsListView;
 
-    //Lista especial do JavaFX que "observa" mduanças na lista, ou seja, quando os itens na lista mudam, os componentes
-    //interface gráfica que usam essa listão serão automaticamente atualizados
+    private ObservableList<Student> studentsList;
 
-    //Utilizada para armezenar dados que serão exibidos em componentes como ListView
-    private ObservableList<String> studentsList;
-
-    private StudentDAOImpl dao;
+    private final StudentDAOImpl dao;
 
     public StudentsController() {
         this.dao = new StudentDAOImpl();
     }
+
     @FXML
-    public void initialize(){
+    public void initialize() {
         studentsList = FXCollections.observableArrayList();
         studentsListView.setItems(studentsList);
+        studentsListView.setCellFactory(new Callback<ListView<Student>, ListCell<Student>>() {
+            @Override
+            public ListCell<Student> call(ListView<Student> param) {
+                return new StudentListCell();
+            }
+        });
         loadStudentsFromDatabase();
     }
+
     private void loadStudentsFromDatabase() {
         List<Student> students = dao.findAll();
-        for(Student student : students){
-            String studentInfo = student.getName() + " | " + student.getPhone() + " | " + student.getDataNasc();
-            studentsList.add(studentInfo);
-        }
+        studentsList.addAll(students);
     }
 
     @FXML
     private void abrirTelaDeCadastro() {
         try {
-            // Carregar o novo arquivo FXML (o arquivo de cadastro do aluno)
             FXMLLoader loader = new FXMLLoader(getClass().getResource("registroAluno.fxml"));
-            Scene novaCena = new Scene(loader.load( ));
+            Scene novaCena = new Scene(loader.load());
 
-            // Obter a janela atual (Stage) e trocar de tela
             Stage stage = (Stage) Registrar.getScene().getWindow();
             stage.setScene(novaCena);
         } catch (IOException e) {
-            e.printStackTrace();  // Exibe a exceção no console para depuração
+            e.printStackTrace();
         }
     }
 }
