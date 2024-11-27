3e859076eebf8f353c875c201961eb07dc6a94d6
Henrique Gouveia <henriquesgo42@gmail.com>
2024-11-27 14:42:25 -0300

    Implementacao da lista renderizavel na listView de estudantes

--- src/main/java/poowyden/poowyden/StudentsController.java
+++ src/main/java/poowyden/poowyden/StudentsController.java
@@ -1,17 +1,52 @@
 package poowyden.poowyden;
 
+import DAO.StudentDAOImpl;
+import Entities.Student;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.collections.ObservableList;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.util.List;
+
 
 public class StudentsController {
 
     // Referência ao botão no FXML
     @FXML
     private Button Registrar;
+
+    @FXML
+    private ListView<String> studentsListView;
+
+    //Lista especial do JavaFX que "observa" mduanças na lista, ou seja, quando os itens na lista mudam, os componentes
+    //interface gráfica que usam essa listão serão automaticamente atualizados
+
+    //Utilizada para armezenar dados que serão exibidos em componentes como ListView
+    private ObservableList<String> studentsList;
+
+    private StudentDAOImpl dao;
+
+    public StudentsController() {
+        this.dao = new StudentDAOImpl();
+    }
+    @FXML
+    public void initialize(){
+        studentsList = FXCollections.observableArrayList();
+        studentsListView.setItems(studentsList);
+        loadStudentsFromDatabase();
+    }
+    private void loadStudentsFromDatabase() {
+        List<Student> students = dao.findAll();
+        for(Student student : students){
+            String studentInfo = student.getName() + " | " + student.getPhone() + " | " + student.getDataNasc();
+            studentsList.add(studentInfo);
+        }
+    }
 
     @FXML
     private void abrirTelaDeCadastro() {
