92821e33f3a4b1920e501b73c4313f39ec822fec
Henrique Gouveia <henriquesgo42@gmail.com>
2024-11-28 17:53:10 -0300

    Criação de novos atributos da entidade Student

--- src/main/java/Entities/Student.java
+++ src/main/java/Entities/Student.java
@@ -1,36 +1,51 @@
 package Entities;
 
+import java.util.Date;
+
 public class Student {
     private int id;
     private String name;
     private String phone;
     private String imgSrc;
     private String dataNasc;
+    private Date registrationDate;
+    private boolean allPaymentsUpToDate; //Indica se todos os pagamentos estão em dia
 
     public int getId() {return id;}
     public void setId(int id) {this.id = id;}
+
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
+
     public String getPhone() {
         return phone;
     }
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
     public String getImgSrc() {
         return imgSrc;
     }
     public void setImgSrc(String imgSrc) {
         this.imgSrc = imgSrc;
     }
+
+    public Date getRegistrationDate() { return registrationDate; }
+    public void setRegistrationDate(Date registrationDate) { this.registrationDate = registrationDate; }
+
     public String getDataNasc() {
         return dataNasc;
     }
     public void setDataNasc(String dataNasc) {
         this.dataNasc = dataNasc;
     }
+
+    public boolean isAllPaymentsUpToDate() { return allPaymentsUpToDate; }
+    public void setAllPaymentsUpToDate(boolean allPaymentsUpToDate) { this.allPaymentsUpToDate = allPaymentsUpToDate; }
+
 }
