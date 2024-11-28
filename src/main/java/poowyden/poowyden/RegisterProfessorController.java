f37a4b6d32e51dfd8f04a8b9b40ebf8244a73751
Henrique Gouveia <henriquesgo42@gmail.com>
2024-11-28 18:08:19 -0300

    Implementacao do metodo openDashboard para voltar para o dashboard já aberto na aba representada por 'professors.fxml'

--- src/main/java/poowyden/poowyden/RegisterProfessorController.java
+++ src/main/java/poowyden/poowyden/RegisterProfessorController.java
@@ -7,6 +7,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class RegisterProfessorController {
 
@@ -45,6 +46,19 @@ public class RegisterProfessorController {
 
     private final ProfessorDAO professorDAO = new ProfessorDAOImpl();
 
+    //Método para cancelar o cadastro e voltar para a tela anterior
+    @FXML
+    //Abre o dashboard já carregado com a aba de alunos
+    private void cancelarCadastro() {
+        System.out.println("Cadastr de professor cancelado");
+        // Obtém o Stage atual (onde o botão "Cancelar" está)
+        Stage currentStage = (Stage) Cancel.getScene().getWindow();
+
+        // Usa a classe BackToDashboard para abrir o dashboard no mesmo Stage, EVITANDO ASSIM ABRIR UMA OUTRA JANELA
+        BackToDashboard backToDashboard = new BackToDashboard();
+        backToDashboard.openDashboard(currentStage, "professors.fxml");
+    }
+
     @FXML
     void cadastrarProfessor(ActionEvent event) {
         String name = nameField.getText();
@@ -73,9 +87,5 @@ public class RegisterProfessorController {
         System.out.println("Registro Professor cadastrado com sucesso");
     }
 
-    @FXML
-    void cancelarCadastro(ActionEvent event) {
-
-    }
 
 }
