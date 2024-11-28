39ff00b623b8c9256279bc4a8ef4b77d10fc0466
Henrique Gouveia <henriquesgo42@gmail.com>
2024-11-28 17:52:09 -0300

    Aprimoramento dos atributos da classe entidade Payment

--- src/main/java/Entities/Payment.java
+++ src/main/java/Entities/Payment.java
@@ -1,12 +1,19 @@
 package Entities;
+
 import java.util.Date;
 
 public class Payment {
     private int id;
-    private Date date;
+    private Date payday;
     private double amount;
-    private String type_plan;
     private int payerId;
+    private String paymentMethod;
+    private int accountId;
+    private String status; //Valores possíveis: "payed", "not_payed"
+    private String type_plan;
+    private Date expirePayDay;
+    private boolean expired;
+
 
     public int getId() {
         return id;
@@ -14,23 +21,17 @@ public class Payment {
     public void setId(int id) {
         this.id = id;
     }
-    public java.sql.Date getDate() {
-        return (java.sql.Date) date;
+    public Date getPayDay() {
+        return payday;
     }
-    public void setDate(Date date) {
-        this.date = date;
+    public void setPayday(Date payday) {
+        this.payday = payday;
     }
-    public float getAmount() {
-        return (float) amount;
+    public double getAmount() {
+        return amount;
     }
     public void setAmount(double amount) {
         this.amount = amount;
-    }
-    public String getType_plan() {
-        return type_plan;
-    }
-    public void setType_plan(String type_plan) {
-        this.type_plan = type_plan;
     }
     public int getPayerId() {
         return payerId;
@@ -38,4 +39,24 @@ public class Payment {
     public void setPayerId(int payerId) {
         this.payerId = payerId;
     }
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+    public int getAccountId() { return accountId; }
+    public void setAccountId(int accountId) { this.accountId = accountId; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+
+    public String getType_plan() { return type_plan; }
+    public void setType_plan(String type_plan) { this.type_plan = type_plan; }
+
+    public Date getExpirePayDay() { return expirePayDay; }
+    public void setExpirePayDay(Date expirePayDay) { this.expirePayDay = expirePayDay; }
+
+    public boolean isExpired() { return expired; }
+    public void setExpired(boolean expired) { this.expired = expired; }
 }
