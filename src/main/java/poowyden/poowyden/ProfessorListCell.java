5a6b5f1b45474b3f3ce264ccf94ed82cd6319c1f
Henrique Gouveia <henriquesgo42@gmail.com>
2024-11-28 10:04:09 -0300

    Implementacao da classe que cria uma única célula a ser exibida no ListView de entities 'Professor's'

--- src/main/java/poowyden/poowyden/ProfessorListCell.java
+++ src/main/java/poowyden/poowyden/ProfessorListCell.java
@@ -1,4 +1,35 @@
 package poowyden.poowyden;
 
-public class ProfessorListCell {
+import Entities.Professor;
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
+//ListCell<T> é uma classe genérica do JavaFX que representa uma única célula em um ListView,
+//T é o tipo de objeto que a célula exibirá
+public class ProfessorListCell extends ListCell<Professor> {
+    private GridPane gridPane = new GridPane();
+    private Text name = new Text();
+    private Text phone = new Text();
+    private Text cpf = new Text();
+
+    public ProfessorListCell() {
+        gridPane.add(name, 0, 0);
+        gridPane.add(phone, 1, 0);
+        gridPane.add(cpf, 2, 0);
+        gridPane.setHgap(50);
+    }
+
+    @Override
+    protected void updateItem(Professor professor, boolean empty) {
+        super.updateItem(professor, empty);
+        if(empty || professor == null) {
+            setGraphic(null);
+        }else{
+            name.setText(professor.getName());
+            phone.setText(professor.getPhone());
+            cpf.setText(professor.getCpf());
+            setGraphic(gridPane);
+        }
+    }
 }
